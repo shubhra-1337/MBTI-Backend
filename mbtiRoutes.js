bea19c82c4420bae3299742659fea927c5c71b4b
shubhra-1337 <shubhra1337@gmail.com>
2025-09-23 23:08:45 +0530

    Update mbtiRoutes.js

--- mbtiRoutes.js
+++ mbtiRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const MBTI = require("../mbtiModel");
+const MBTI = require("/mbtiModel");
 
 router.get("/:type", async (req, res) => {
   try {
@@ -20,4 +20,5 @@ router.get("/:type", async (req, res) => {
 });
 
 module.exports = router;
+
 
